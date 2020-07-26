3ae8cac6338dd233c015e22089e4b966b40584b8
Ashley Phillips <ashley.phillips@nodeocrat.com>
2020-07-26 22:30:52 +0100

    remove backups

--- backup/saveMemappData.sh
+++ backup/saveMemappData.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 MONGO_DB_NAME="memapp-test-1"
-DATA_NAME="memapp-data-2020-07-24"
+DATA_NAME="memapp-data-2020-07-26"
 
 docker exec ${MONGO_DB_NAME} mongodump --quiet --out /tmp/${DATA_NAME}
 docker cp ${MONGO_DB_NAME}:/tmp/${DATA_NAME} ./${DATA_NAME}
