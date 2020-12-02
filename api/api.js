90750dc57538b3ce2d6747bba05f7870c47fb33e
Ashley Phillips <ashley.phillips@nodeocrat.com>
2020-12-02 16:29:44 +0000

    Fix course tag creation

--- api/api.js
+++ api/api.js
@@ -287,7 +287,7 @@ router.post('/course/:courseId/tag/create', async (req, res) => {
   const newTag = req.body.tag;
 
   if (!courseId) {
-    res.status(400).json();
+    return res.status(400).json();
   }
 
   if (!user.courses[courseId]) {
@@ -300,7 +300,7 @@ router.post('/course/:courseId/tag/create', async (req, res) => {
   } else if (!tags.includes(newTag)) {
     tags.push(newTag);
   } else {
-    res.status(400).json({ msg: 'tag already exists' });
+    return res.status(400).json({ msg: 'tag already exists' });
   }
 
   user.markModified('courses');
@@ -310,11 +310,15 @@ router.post('/course/:courseId/tag/create', async (req, res) => {
 });
 
 router.post('/course/:courseId/course-tag/create', async (req, res) => {
-  const courseId = req.courseId;
+  const courseId = req.params.courseId;
   const newTag = req.body.tag;
 
-  if (!newTag || !courseId) {
-    res.status(400).json();
+  if (!newTag) {
+    return res.status(400).json({ error: 'No tag given' });
+  }
+
+  if (!courseId) {
+    return res.status(400).json({ error: 'Course ID not found' });
   }
 
   const updatedCourse = await Course.findOneAndUpdate(
@@ -324,10 +328,10 @@ router.post('/course/:courseId/course-tag/create', async (req, res) => {
   );
 
   if (!updatedCourse) {
-    res.status(400).json();
+    return res.status(400).json();
   }
 
-  res.json(updatedCourse);
+  return res.json(updatedCourse);
 });
 
 /**
