c94b982bfb55379b76e292052618b4e1e26fbba1
Ashley Phillips <ashley.phillips@nodeocrat.com>
2022-02-13 22:11:14 +0000

    backup feb 2022

--- backup/saveMemappData.sh
+++ backup/saveMemappData.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 MONGO_DB_NAME="memapp-test-1"
-DATA_NAME="memapp-data-2021-07-25"
+DATA_NAME="memapp-data-2022-02-13"
 
 docker exec ${MONGO_DB_NAME} mongodump --quiet --out /tmp/${DATA_NAME}
 docker cp ${MONGO_DB_NAME}:/tmp/${DATA_NAME} ./${DATA_NAME}
